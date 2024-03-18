6e9e2df80550d7c3a7daabce23ca464cf533d5b2
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-03-18 09:29:03 +0200

    add unit tests for read_file_pandas

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -1,4 +1,5 @@
 import unittest
+import pandas as pd
 from app.io.input import read_file, read_file_pandas
 
 
@@ -14,6 +15,18 @@ class TestInputFunctions(unittest.TestCase):
     def test_read_file_empty_success(self):
         result = read_file("./data/in/empty.csv")
         self.assertEqual(result, "")
+
+    def test_read_file_pandas_success(self):
+        result = read_file_pandas("./data/in/test_data.csv")
+        self.assertEqual(result, ",1, 2, 3\r\n")
+
+    def test_read_file_pandas_fail(self):
+        with self.assertRaises(FileNotFoundError):
+            read_file_pandas("./data/in/test_data1.csv")
+
+    def test_read_file_pandas_empty_fail(self):
+        with self.assertRaises(pd.errors.EmptyDataError):
+            read_file_pandas("./data/in/empty.csv")
 
 
 if __name__ == '__main__':
